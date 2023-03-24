b36ce6029f729163e295750d5eb9ffbb8ba34996
AswinSatheesh <aswinsatheesh2001@gmail.com>
2023-03-24 23:26:55 +0530

    spring boot crud operation done

--- spring-boot-example/src/main/java/com/aswin/Main.java
+++ spring-boot-example/src/main/java/com/aswin/Main.java
@@ -2,38 +2,74 @@ package com.aswin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet()
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age)
     {
-        GreetResponse response = new GreetResponse(
-                "HELLO, THIS IS ASWIN",
-                List.of("Java", "Python", "JavaScript"),
-                new Person("Aswin",22,  45_000)
-        );
-        return response;
-    }
-
-    record  Person(String name,int age,double savings){
 
     }
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-    ){}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setAge(request.age());
+        customer.setEmail(request.email());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+//    @GetMapping("/greet")
+//    public GreetResponse greet()
+//    {
+//        GreetResponse response = new GreetResponse(
+//                "HELLO, THIS IS ASWIN",
+//                List.of("Java", "Python", "JavaScript"),
+//                new Person("Aswin",22,  45_000)
+//        );
+//        return response;
+//    }
+//
+//    record  Person(String name,int age,double savings){
+//
+//    }
+//    record GreetResponse(
+//            String greet,
+//            List<String> favProgrammingLanguages,
+//            Person person
+//    ){}
+
 
 
 //    class GreetResponse{
@@ -67,4 +103,5 @@ public class Main {
 //        return Objects.hash(greet);
 //    }
 //}
+
 }
